7c50c884c081a8de1bbe60a7aeec36f5509fefb0
Victor Boehm <victor.b.assumpcao@aluno.senai.br>
2025-05-30 08:55:34 -0300

    Adiciona novas tarefas

---
+++ tarefas.js
@@ -0,0 +1,25 @@
+//Etapa 1
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+tarefas.push(`Ir ao médico`, `Comprar pão`);
+
+console.log(`Com as tarefas novas:`, tarefas);
+
+//Etapa 2
+let ultimaTarefaRemovida = tarefas.pop();
+
+console.log(`Tarefa removida: ${ultimaTarefaRemovida}`);
+
+console.log(`Depois de remover a tarefa: ${tarefas}`);
+
+//Etapa 3
+
+tarefas.unshift(`Assistir um filme`);
+
+console.log(`Depois de adicionar uma nova tarefa ${tarefas}`);
+
+//Etapa 4 
+
+tarefas.shift();
+
+console.log(`Com a primeira tarefa removida: ${tarefas}`);
